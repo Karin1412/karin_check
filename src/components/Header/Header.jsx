0978d950508550e4c5a31091ec12007b51809005
Karin1412 <100667390+Karin1412@users.noreply.github.com>
2024-06-08 05:00:08 +0700

    header

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import './Header.css'
+export default function Header() {
+  return (
+    <header className='p-6 mx-auto'>
+        <nav className='flex flex-row justify-between items-center'>
+            <div className='basis-2/6 text-center font-bold text-2xl cursor-pointer'>
+                Karin.
+            </div>
+
+            <ul className='basis-3/6 flex justify-end items-center gap-8 font-medium text-sm uppercase'>
+                <li className='header-li'>home</li>
+                <li className='header-li'>skill</li>
+                <li className='header-li'>project</li>
+            </ul>
+
+            <button className='basis-1/6 ml-10 p-2 flex justify-center items-center'>
+              <span className='inline-block p-2 border-2 border-solid border-amber-700 font-semibold text-amber-700 hover:bg-amber-700 hover:text-black'>
+                CONTACT ME
+              </span>
+            </button>
+            
+        </nav>
+    </header>
+  )
+}
